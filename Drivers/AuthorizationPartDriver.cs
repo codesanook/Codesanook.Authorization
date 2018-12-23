f56bd97bddaaa40c8315039b7a911a051f39427a
aaronamm <theeranitp@gmail.com>
2018-12-23 17:12:48 +0700

    Add comment to AuthorizationPartDriver

--- Drivers/AuthorizationPartDriver.cs
+++ Drivers/AuthorizationPartDriver.cs
@@ -3,8 +3,10 @@ using Orchard.ContentManagement.Drivers;
 
 namespace CodeSanook.Authorization
 {
+    /// <summary>
+    /// This just a requirement to make attached part work
+    /// </summary>
     public class AuthorizationPartDriver : ContentPartDriver<AuthorizationPart>
     {
-
     }
 }
